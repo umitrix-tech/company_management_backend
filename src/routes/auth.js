a2f5f5c6b6247a7fba54a47b1d4e60f366134e72
sakthi <sakthi@umitrix.in>
2025-12-08 10:50:21 +0530

    test sample

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const validate = require("../validation");
 const { authValidator } = require("../validation/auth.validation");
 const { loginController, registerController } = require("../controller/auth.controller");
+const prisma = require("../../prisma");
 
 router.post("/login", validate(authValidator.login), loginController);
 router.post("/register", validate(authValidator.register), registerController);
+router.get('/get', async (req,res)  => {
+
+    let s = await prisma.user.findMany({});
+
+    return {
+        reslt:s,
+        messsage:"Sucucse"
+    }
+    
+})
 
 module.exports = router;
 
