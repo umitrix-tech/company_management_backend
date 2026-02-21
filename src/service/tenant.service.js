5d6258f25a7b4b1e7be6ac256fa8a7fe58a8fc18
sakthi <sakthi@umitrix.in>
2026-02-21 19:41:16 +0530

    host domain

--- src/service/tenant.service.js
+++ src/service/tenant.service.js
@@ -5,11 +5,20 @@ const catchAsyncPrismaError = require("../utils/catchAsyncPrismaError");
 
 const tenantConfigService = async (req) => {
     try {
+
+        if (!req.headers?.hostdomain) {
+            throw new AppError("Host domain is required", 400);
+        }
+
         const responce = await prisma.companyConfig.findFirst({
             where: {
                 domain: String(req.headers?.hostdomain)
             }
         });
+
+        if (!responce) {
+            throw new AppError("Host domain not found", 404);
+        }
                 
         return responce;
     } catch (error) {
